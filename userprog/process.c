02398d714e7af27b8a4278ebf9ac0c3add72abd6
taehoy <rethnoly@gmail.com>
2024-11-13 09:50:56 +0000

    feat: Argument Parcing 구현, Fix USERPROG

--- userprog/process.c
+++ userprog/process.c
@@ -1,3 +1,4 @@
+#define USERPROG
 #include "userprog/process.h"
 #include <debug.h>
 #include <inttypes.h>
@@ -49,6 +50,10 @@ process_create_initd (const char *file_name) {
 	if (fn_copy == NULL)
 		return TID_ERROR;
 	strlcpy (fn_copy, file_name, PGSIZE);
+
+	/* === project2 - Command Line Parsing === */
+	// char *ptr;
+    // strtok_r(file_name, " ", &ptr);
 
 	/* Create a new thread to execute FILE_NAME. */
 	tid = thread_create (file_name, PRI_DEFAULT, initd, fn_copy);
@@ -176,13 +181,28 @@ process_exec (void *f_name) {
 	/* We first kill the current context */
 	process_cleanup ();
 
+	/* === project2 - Command Line Parsing === */
+	char *ptr, *arg;
+	int arg_cnt = 0;
+	char *arg_list[32];
+
+	for (arg = strtok_r(file_name, " ", &ptr); arg != NULL; arg = strtok_r(NULL, " ", &ptr))
+        arg_list[arg_cnt++] = arg;
+
 	/* And then load the binary */
-	success = load (file_name, &_if);
+	success = load(file_name, &_if);
+
+	/* === project2 - Command Line Parsing === */
+	argument_stack(arg_list, arg_cnt, &_if);
 
 	/* If load failed, quit. */
 	palloc_free_page (file_name);
 	if (!success)
 		return -1;
+
+	/* === project2 - Command Line Parsing === */
+	hex_dump(_if.rsp, _if.rsp, USER_STACK - _if.rsp, true); // 0x47480000	
+
 
 	/* Start switched process. */
 	do_iret (&_if);
@@ -204,6 +224,10 @@ process_wait (tid_t child_tid UNUSED) {
 	/* XXX: Hint) The pintos exit if process_wait (initd), we recommend you
 	 * XXX:       to add infinite loop here before
 	 * XXX:       implementing the process_wait. */
+
+	/* === project2 - Command Line Parsing === */
+	while(1)
+		;
 	return -1;
 }
 
@@ -421,7 +445,7 @@ load (const char *file_name, struct intr_frame *if_) {
 
 done:
 	/* We arrive here whether the load is successful or not. */
-	file_close (file);
+	// file_close (file);
 	return success;
 }
 
@@ -569,6 +593,38 @@ install_page (void *upage, void *kpage, bool writable) {
 	return (pml4_get_page (t->pml4, upage) == NULL
 			&& pml4_set_page (t->pml4, upage, kpage, writable));
 }
+
+/* === project2 - Command Line Parsing === */
+void argument_stack(char **argv, int argc, struct intr_frame *if_)
+{
+	char *arg_addr[100];
+    int argv_len;
+
+    for (int i = argc - 1; i >= 0; i--) {
+        argv_len = strlen(argv[i]) + 1;
+        if_->rsp -= argv_len;
+        memcpy(if_->rsp, argv[i], argv_len);
+        arg_addr[i] = if_->rsp;
+    }
+
+    while (!(if_->rsp % 8))
+        *(uint8_t *)(--if_->rsp) = 0;
+
+    for (int i = argc; i >= 0; i--) {
+        if_->rsp = if_->rsp - 8;
+        if (i == argc)
+            memset(if_->rsp, 0, sizeof(char **));
+        else
+            memcpy(if_->rsp, &arg_addr[i], sizeof(char **));
+    }
+
+    if_->rsp = if_->rsp - 8;
+    memset(if_->rsp, 0, sizeof(void *));
+
+    if_->R.rdi = argc;
+    if_->R.rsi = if_->rsp + 8;
+}
+
 #else
 /* From here, codes will be used after project 3.
  * If you want to implement the function for only project 2, implement it on the
