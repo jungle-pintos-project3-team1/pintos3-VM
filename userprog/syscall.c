5722d8dff54227cc32f53bc0a0b526c1261f9e6f
taehoy <rethnoly@gmail.com>
2024-11-16 05:19:59 +0000

    Feat: pintos - system call ~remove 구현

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -8,6 +8,10 @@
 #include "threads/flags.h"
 #include "intrinsic.h"
 
+/** #Project 2: System Call */
+#include "filesys/filesys.h"
+
+
 void syscall_entry (void);
 void syscall_handler (struct intr_frame *);
 
@@ -41,6 +45,84 @@ syscall_init (void) {
 void
 syscall_handler (struct intr_frame *f UNUSED) {
 	// TODO: Your implementation goes here.
-	printf ("system call!\n");
-	thread_exit ();
+	int sys_number = f->R.rax; // rax에 있는 시스템 콜 번호 추출
+
+	switch(sys_number)
+	{
+		case SYS_HALT:
+			half();
+			break;
+		case SYS_EXIT:
+			exit(f->R.rdi);
+			break;
+		case SYS_FORK:
+			f->R.rax = fort(f->R.rdi);
+			break;
+		case SYS_EXEC:
+			f->R.rax = exec(f->R.rdi);
+			break;
+		case SYS_WAIT:
+			f->R.rax = process_wait(f->R.rdi);
+			break;
+		case SYS_CREATE :
+			f->R.rax = create(f->R.rdi, f->R.rsi);
+			break;
+		case SYS_REMOVE:
+			f->R.rax = remove(f->R.rdi);
+			break;
+		case SYS_OPEN:
+			f->R.rax = open(f->R.rdi);
+			break;
+		case SYS_FILESIZE:
+			f->R.rax = filesize(f->R.rdi);
+			break;
+			// 브레이크땐스 시작
+		case SYS_READ:
+			f->R.rax = read(f->R.rdi, f->R.rsi, f->R.rdx);
+			break;
+		case SYS_WRITE:
+			f->R.rax = write(f->R.rdi, f->R.rsi, f->R.rdx);
+			break;
+		case SYS_SEEK:
+			seek(f->R.rdi, f->R.rsi);
+			break;
+		case SYS_TELL:
+			f->R.rax = tell(f->R.rdi);
+			break;
+		default:
+			exit(-1);
+		}
+}
+
+void check_address(void *addr) {
+	/* 유저 영역이 아닌 경우 프로세스 종료 */
+	if (is_kernel_vaddr(addr) || addr == NULL || pml4_get_page(thread_current() -> pml4, addr) == NULL)
+		exit(-1);
+}
+
+/* 핀토스를 종료시키는 시스템 콜 */
+void halt(void){
+	power_off();
+}
+
+/* 현재 프로세스를 종료시키는 시스템 콜 */
+void exit(int status){
+	struct thread *t = thread_current();
+	t->exit_status = status;
+	printf("%s: exit(%d)\n", t->name, t->exit_status); // Process Tremination Message
+	thread_eixt();
+}
+
+/* 파일을 생성하는 시스템 콜 */
+bool create(const char *file, unsigned initial_size){
+	check_address(file);
+
+	return filesys_create(file, initial_size);
+}
+
+/* 파일을 삭제하는 시스템 콜 */
+bool remove(const char *file){
+	check_address(file);
+
+	return filesys_remove(file);
 }
