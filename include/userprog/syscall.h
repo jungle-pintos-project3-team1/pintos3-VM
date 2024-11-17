acef2e4170cd44bc604d4ce3aaaa1ef5e26ddf71
taehoy <rethnoly@gmail.com>
2024-11-17 10:41:34 +0000

    Feat: 파일 관련 시스템콜 선언

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -9,6 +9,12 @@ void halt(void);
 void exit(int status);
 bool create(const char *file, unsigned initial_size);
 bool remove(const char *file);
+int open(const char *file);
+int filesize(int fd);
+int read(int fd, void *buffer, unsigned length);
+int write(int fd, const void *buffer, unsigned length);
+int tell(int fd);
+void close(int fd);
 
-void syscall_init (void);
+void syscall_init(void);
 #endif /* userprog/syscall.h */
